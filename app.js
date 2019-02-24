02e93168b2bbbc1137ac8aab726713ed67f60f37
Nathan <nathanlui429@gmail.com>
2019-02-24 18:29:25 -0500

    final

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 //define variable for app
 const app = {};
-let lang = '';
 const searchUrl = 'https://restcountries.eu/rest/v2/lang/';
 let finalUrl = '';
 
+// STRETCH GOAL ATTEMPTS//
+// app.getCountriesByName = function (results){
+//   const alphaCode = results.map((index)=>{
+//     return index.alpha2Code;
+//   })
+//   console.log(alphaCode)
+// }
 
 app.getCountryByCode = function() {
   $('#language').change(function(){
     let code = $('#language option:selected').attr("value");
     app.apiUrl = searchUrl + code;
   
+    // make AJAX request to get API data for countries //
     $.ajax({
       url: app.apiUrl,
       method: 'GET',
@@ -17,6 +24,7 @@ app.getCountryByCode = function() {
       }).then((result) => {
         $('ul').empty();
         app.countriesByLanguage(result);
+        app.getCountriesByName(result);
       })  
   }); 
 }
@@ -24,31 +32,20 @@ app.getCountryByCode = function() {
 
 app.init = function(){
   app.getCountryByCode();
-  // app.displayCountries();
   
   // MAP THINGG //
-  // $('#vmap').vectorMap(
-  // {
-  //   map: 'world_en' 
-  // })
+  $('#vmap').vectorMap(
+  {
+    map: 'world_en',
+      hoverOpacity: 0.7,
+      backgroundColor: 'rgba(0, 0, 0, 0)',
+      color: '#fff',
+      enableZoom: false,
+  })
 };
 
-
-// create variable for language endpoint
-// link appropriate code with dropdown menu //
-
-
-//make an AJAX request to get API data for countries
-
-
-
 app.countriesByLanguage = (countries) => {
-  // const countryList = countries.map((countries) => {
-    
-  //     console.log(countries.name, countries.flag, countries.region, countries.population);
-  // });
   countries.forEach(function(result){
-      console.log(result);
     let country = (result.name);
     let region = (result.region);
     let flag = (result.flag);
@@ -63,71 +60,8 @@ app.countriesByLanguage = (countries) => {
         <h3>Population: ${population}</h3>
         <h3>Capital: ${capital}</h3>   
       </li>`);
-      // let country = $('.countryName');
-      // let region = $('.region');
-      
-      // country.html(result.name)
-      // region.html(result.region)
-
-//each country will be an li
-
-      // console.log(result.name)
-    //   $('.results').html(`
-    //     <div>${result.flag}</div>
-    //     <h2>${result.name}</h2>
-    //     <h3>${result.region}</h3>
-    //     <p>${result.population}</p>`)
     })
   };
-// const {countryName, countryFlag, countryRegion, countryPopulation} = app.countriesByLanguage;
-
-// app.displayResult = (userChoice) => {
-//   const resultList = userChoice.filter((index) => {
-//     return index;
-//   });
-//   resultList.forEach((result) => {
-//     $('.results').append(`
-//         <div>${result.flag}</div>
-//         <h2>${result.name}</h2>
-//         <h3>${result.region}</h3>
-//         <p>${result.population}</p>
-//         `)
-//   })
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-//   app.countryFlags = (flags) => {
-//     const flagList = countries.map((flags) => {
-//       console.log(countries.flag);
-//     });
-   
-//    // filter the array for our desired items based on LANGUAGE
-//   //  countries.filter()
-// };
-
-// app.countriesByFlag = (countries) => {
-//   const countryList = countries.map((countries) => {
-//     return countries.name;
-//   });
-// };
-  
-//we will use the search parameters to categorize countries by language 
-
-//the user will select the language by dropdown menu to filter countries
-
-//the selected country info will be displayed, specifically: country name, flag, language, population, region/continent
-
-//when the user switches languages, the new data will be displayed and will replace the previous language data
 
 $(document).ready(function () {
   app.init();  
